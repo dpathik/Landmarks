ed0c99b9880a06642e9e5b7df411bd07ecde11cc
Pathik Desai <dpathik@gmail.com>
2020-01-02 15:55:44 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Pathik Desai on 1/2/20.
+//  Copyright © 2020 TriNetX Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

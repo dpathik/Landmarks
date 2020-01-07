c222831e8b64573976aa2a5c0753cd49e438a5b6
Pathik Desai <dpathik@gmail.com>
2020-01-06 21:08:01 -0500

    Adding Landmark detail view to Scene Delegate

---
+++ Landmarks/LandmarkDetail.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Pathik Desai on 1/6/20.
+//  Copyright © 2020 TriNetX Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
